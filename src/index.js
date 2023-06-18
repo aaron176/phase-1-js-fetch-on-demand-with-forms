df191f88093deb9bb75651de41cfb19afeea3a03
haroon <haroon.seena2017@gmail.com>
2023-06-18 14:19:38 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
 const init = () => {
-  
+ 
+  const form = document.querySelector("form")
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const input =  document.getElementById("searchByID")
+    fetch(`http://localhost:3000/movies/${input.value}`)
+    .then(res => res.json())
+    .then(data => {
+        const title = document.querySelector("section#movieDetails h4")
+        const paragraph = document.querySelector("section#movieDetails p")
+
+        title.innerHTML = data.title;
+        paragraph.innerHTML = data.summary
+    });
+});
 }
 
 document.addEventListener('DOMContentLoaded', init);
